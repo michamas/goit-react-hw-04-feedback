a6a6bd668f23bbf260bc5892c18fcb6bab1fec70
Michał Masiulanis <m.masiulanis@gmail.com>
2023-03-21 19:37:07 +0100

    counter

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,16 +1,34 @@
-const { Component } = require('react');
+import { Component } from 'react';
 
 class Counter extends Component {
   static defaultProps = {
     step: 1,
+    initialValue: 0,
+  };
+
+  state = {
+    value: this.props.initialValue,
   };
 
   static propTypes = {};
 
-  handleIncrement(event) {
+  constructor(props) {
+    super(props);
+
+    //decrement rozpisany w jeden sposob; increment w drugi - lepszy
+    this.handleDecrement = this.handleDecrement.bind(this);
+    // this.handleIncrement = this.handleIncrement.bind(this);
+
+    // stan deklaruje sie w konstrktorze lub jako publiczna wlasciwosc klasy (Ln 9)
+    // this.state = {
+    //   value: this.props.initialValue,
+    // };
+  }
+
+  handleIncrement = event => {
     console.log('Increment btn was clicked', event);
     console.log('this.props: ', this.props);
-  }
+  };
   handleDecrement(event) {
     console.log('Decrement btn was clicked', event);
     console.log('this.props: ', this.props);
@@ -22,7 +40,7 @@ class Counter extends Component {
     return (
       <div>
         <div>COUNTER</div>
-        <span>0</span>
+        <span>{this.state.value}</span>
         <button type="button" onClick={this.handleIncrement}>
           Increment by {step}
         </button>
